386ebacb59776aa311b21d44d3f676681b5b38d7
Dinithi1625403 <dananjanakadd.23@uom.lk>
2026-02-15 08:49:22 +0530

    initial commit

---
+++ src/main/java/com/fixzone/fixzon_backend/FixzonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fixzone.fixzon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixzonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixzonBackendApplication.class, args);
+	}
+
+}
