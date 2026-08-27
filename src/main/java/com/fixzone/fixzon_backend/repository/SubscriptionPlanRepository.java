0ec914034f175ef6966f96e4f85677cd896a09e2
Chamathka <rjklchamathkaranathunga@gmail.com>
2026-08-27 20:40:15 +0530

    Update subscription service and frontend UI

--- src/main/java/com/fixzone/fixzon_backend/repository/SubscriptionPlanRepository.java
+++ src/main/java/com/fixzone/fixzon_backend/repository/SubscriptionPlanRepository.java
@@ -2,8 +2,10 @@ package com.fixzone.fixzon_backend.repository;
 
 import com.fixzone.fixzon_backend.model.SubscriptionPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
 public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, UUID> {
     boolean existsByName(String name);
+    List<SubscriptionPlan> findByIsActiveTrue();
 }
